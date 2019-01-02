1227a9a551e6b918a413c6282d88dc18ddc3ec6b
visualhatch <rujpYf-wujcur-taqxo6>
2019-01-01 18:16:45 -0600

    First Commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import {createStore} from "redux/es/redux";
+import './index.css';
+import App from './containers/App';
+
+import {searchRobots} from "./reducers";
+
+import * as serviceWorker from './serviceWorker';
+import 'tachyons';
+
+
+
+const store = createStore(searchRobots);
+
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App store={{store}}/>, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
---
+++ src/robots.js
@@ -0,0 +1,50 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Dave Evans',
+        username: 'devans',
+        email: 'dave@davelx.co.uk'
+    },
+
+    {
+        id: 2,
+        name: 'Ashlee Evans',
+        username: 'aevans',
+        email: 'ashlee.macphee@gmail.com'
+    },
+
+    {
+        id: 3,
+        name: 'Gill Evans',
+        username: 'gevans',
+        email: 'gille@icloud.com'
+    },
+
+    {
+        id: 4,
+        name: 'Karen Christy',
+        username: 'kchristy',
+        email: 'karen@sky.com'
+    },
+
+    {
+        id: 5,
+        name: 'Jim Christy',
+        username: 'jchristy',
+        email: 'jim@sky.com'
+    },
+
+    {
+        id: 6,
+        name: 'Charlotte Christy',
+        username: 'cchristy',
+        email: 'charlotte@mcdonalds.com'
+    },
+
+    {
+        id: 7,
+        name: 'Chloe Christy',
+        username: 'chloe',
+        email: 'chloe@kfc.com'
+    },
+];
